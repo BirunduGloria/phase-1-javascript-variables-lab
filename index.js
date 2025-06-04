5c796d2f0d4180bc14a3715beac5a6064f0262cb
Gloria Birundu <birundugloria@gmail.com>
2025-06-04 12:00:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 let companyName = 'Scuber';
-const companyName = 'Scuber';
-// Code your solution in this file!
+const companyName='Scruber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
+
+
+
